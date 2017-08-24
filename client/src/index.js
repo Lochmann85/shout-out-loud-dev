ffd883e4e56ff03f8589da9a2e986361078e86e1
georg lochmann <georg.lochmann@gmail.com>
2017-08-24 17:23:53 +0200

    add react router

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 
 import { ApolloProvider } from 'react-apollo';
 
 import apolloClient from './storeHandler/apolloClient';
+import routerHistory from './storeHandler/routerHistory';
 
 import App from './App';
 
 ReactDOM.render((
    <ApolloProvider client={apolloClient}>
-      <App />
+      <Router history={routerHistory}>
+         <App />
+      </Router>
    </ApolloProvider>
 ), document.getElementById("root"));
