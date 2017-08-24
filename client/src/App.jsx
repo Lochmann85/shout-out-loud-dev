dcc344fa78e0848128aaf5606a126f6c902e341b
georg lochmann <georg.lochmann@gmail.com>
2017-08-24 17:25:58 +0200

    use wrapper and navigation in app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 
 import 'semantic-ui-css/semantic.min.css';
+import './helper/initialization';
 
 import { Grid } from 'semantic-ui-react';
 
+import { FullHeightWrapper } from './assets/styled/Wrapper';
+import Navigation from './components/navigation/Navigation';
+
 const App = () => (
-   <Grid>
-      <Grid.Row>
-         <Grid.Column width={2} />
-         <Grid.Column width={12}>
-            Test
-         </Grid.Column>
-      </Grid.Row>
-   </Grid>
+   <FullHeightWrapper>
+      <Navigation />
+      <Grid>
+         <Grid.Row>
+            <Grid.Column width={2} />
+            <Grid.Column width={12}>Test</Grid.Column>
+         </Grid.Row>
+      </Grid>
+   </FullHeightWrapper>
 );
 
 export default App;
