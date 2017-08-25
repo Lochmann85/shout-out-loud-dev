a3f2201b2c15b29aa99d6cdbc1b100940d217d1a
georg lochmann <georg.lochmann@gmail.com>
2017-08-25 19:41:55 +0200

    add basic push of new shouts

--- server/lib/graphQLApi/services/ShoutsService/index.js
+++ server/lib/graphQLApi/services/ShoutsService/index.js
@@ -16,20 +16,44 @@ const shouts = [
 ];
 
 const types = `
-      type Shout {
-         message: String!
-         type: String!
-      }
-   `;
+type Shout {
+   message: String!
+   type: String!
+}
+input ShoutInput {
+   message: String
+}
+`;
 
 const queries = `
-      getShoutsQueue: [Shout!]!
- `;
+getShoutsQueue: [Shout!]!
+`;
 
 const _queriesResolver = {
    Query: {
       getShoutsQueue() {
          return shouts;
+      },
+   }
+};
+
+const mutations = `
+pushShout(shout: ShoutInput!): Boolean
+`;
+
+const _mutationsResolver = {
+   Mutation: {
+      pushShout(_, { shout }) {
+         return new Promise((resolve, reject) => {
+            if (shout && shout.message) {
+               shout.type = "Custom";
+               shouts.push(shout);
+               resolve(true);
+            }
+            else {
+               reject(`Please enter a message`);
+            }
+         });
       },
    }
 };
@@ -42,10 +66,12 @@ const _queriesResolver = {
  */
 const addResolversTo = (executableSchema) => {
    addResolveFunctionsToSchema(executableSchema, _queriesResolver);
+   addResolveFunctionsToSchema(executableSchema, _mutationsResolver);
 };
 
 export {
    types,
    queries,
+   mutations,
    addResolversTo,
 };
