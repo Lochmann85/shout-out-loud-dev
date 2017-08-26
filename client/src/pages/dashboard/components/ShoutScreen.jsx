c268a250f8442bdb37a48b8e628c700c5444eca8
georg lochmann <georg.lochmann@gmail.com>
2017-08-26 20:31:53 +0200

    add webkit shadow to shout screen

--- client/src/pages/dashboard/components/ShoutScreen.jsx
+++ client/src/pages/dashboard/components/ShoutScreen.jsx
@@ -6,10 +6,13 @@ import colors from './../../../assets/colors/shout-out-loud-colors.json';
 const ShoutScreenBackground = styled.div`
 background-color:${colors.screenBackground};
 height:60%;
+-webkit-box-shadow:inset 0 0 15px 3px ${colors.globalBackground};
+-moz-box-shadow:inset 0 0 15px 3px ${colors.globalBackground};
 box-shadow:inset 0 0 15px 3px ${colors.globalBackground};
 margin:0 -1rem 6% -1rem;
-padding:5rem;
+padding:4rem;
 border:1px solid ${colors.logoDarkerBackground};
+-webkit-border-radius:2rem;
 border-radius:2rem;
 `;
 
@@ -17,7 +20,10 @@ const ShoutScreen = styled.div`
 background-color:${colors.logoLighterBackground};
 height:100%;
 width:100%;
+-webkit-box-shadow: 0 0 20px 20px ${colors.logoLighterBackground};
+-moz-box-shadow: 0 0 20px 20px ${colors.logoLighterBackground};
 box-shadow:0 0 20px 20px ${colors.logoLighterBackground};
+-webkit-border-radius:0.9rem;
 border-radius:0.9rem;
 `;
 
