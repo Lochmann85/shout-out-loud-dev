cd62f8fc3f5560db2891612363762d2f14a397c7
georg lochmann <georg.lochmann@gmail.com>
2017-08-26 16:17:50 +0200

    add first try for shout preview

---
+++ client/src/pages/dashboard/components/ShoutPreview.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import colors from './../../../assets/colors/shout-out-loud-colors.json';
+
+const Background = styled.div`
+   width:100%;
+   background-color:${colors.logoDarkerBackground};
+   color:${colors.logoLighterBackground};
+   border: 1px solid ${colors.logoLighterBackground};
+   -webkit-border-radius:0.28571429rem;
+   border-radius:0.28571429rem;
+   padding:0.5rem;
+   height:50px;
+`;
+
+class SoutPreview extends React.Component {
+
+   render() {
+      const { shout } = this.props;
+
+      return (
+         <Background>
+            {shout.message}
+         </Background>
+      );
+   }
+};
+
+export default SoutPreview;
