4d54061beffd071ab3750975d8722367d04f6ce1
georg lochmann <georg.lochmann@gmail.com>
2017-08-30 11:51:25 +0200

    better deployment

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,15 @@
 # copy new packages by hand
 
+rm -r ../shout-out-loud-deploy/server/build/* ../shout-out-loud-deploy/client/build/*
+
+cd server/
+npm run build
+cd ..
 cp -r server/build/* ../shout-out-loud-deploy/server/build/
 
+cd client/
+npm run build
+cd ..
 cp -r client/build/* ../shout-out-loud-deploy/client/build/
+
+rm -r server/build client/build
