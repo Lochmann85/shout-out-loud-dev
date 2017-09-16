93a1ca1b23af0ca01bc952eaf1eaf3ff43df262a
georg lochmann <georg.lochmann@gmail.com>
2017-09-16 15:18:00 +0200

    add configurations with mongodb

--- server/lib/configurations.js
+++ server/lib/configurations.js
@@ -1,7 +1,28 @@
 const TIMER_INTERVAL = 7000,
    MAX_SHOWN_SHOUTS = 100;
 
+const serverConfig = {
+   OPENSHIFT_PORT: process.env.OPENSHIFT_NODEJS_PORT || 8080,
+   OPENSHIFT_IP: process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0",
+   MONGO_DB_URI: process.env.OPENSHIFT_MONGODB_DB_URL || "127.0.0.1",
+};
+
+console.log(process.env);
+
+if (process.env.DATABASE_SERVICE_NAME) {
+   const mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
+   serverConfig.MONGO_DB_NAME = process.env[mongoServiceName + "_DATABASE"];
+   serverConfig.MONGO_USER_NAME = process.env[mongoServiceName + "_USER"];
+   serverConfig.MONGO_USER_PWD = process.env[mongoServiceName + "_PASSWORD"];
+}
+else {
+   serverConfig.MONGO_DB_NAME = "sotDb";
+   serverConfig.MONGO_USER_NAME = "shout-out-loud";
+   serverConfig.MONGO_USER_PWD = "storage";
+}
+
 export {
+   serverConfig,
    TIMER_INTERVAL,
-   MAX_SHOWN_SHOUTS
+   MAX_SHOWN_SHOUTS,
 };
