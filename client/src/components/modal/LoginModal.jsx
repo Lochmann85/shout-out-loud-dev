6b2ae349fc4e543e2eb866997f35bf6a64e89568
georg lochmann <georg.lochmann@gmail.com>
2017-12-30 13:21:17 +0100

    use forgot password form in login modal

--- client/src/components/modal/LoginModal.jsx
+++ client/src/components/modal/LoginModal.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { compose } from 'react-apollo';
 
 import { Modal, Icon } from 'semantic-ui-react';
 
 import LoginForm from './components/LoginForm';
+import ForgotPasswordForm from './components/ForgotPasswordForm';
 import loginMutation from './graphql/mutations/loginMutation';
+import forgotPasswordMutation from './graphql/mutations/forgotPasswordMutation';
 import mutationErrorHandling from './../../components/errorHandling/mutationErrorHandling';
 
 class LoginModal extends React.Component {
@@ -18,24 +21,55 @@ class LoginModal extends React.Component {
       super(props);
 
       this.state = {
-         errors: []
+         errors: [],
+         isLogin: true,
+         forgotPasswordSuccess: false,
       };
    }
 
    render() {
-      const { onCloseClick, login, onLoginSuccess, ...others } = this.props; //eslint-disable-line no-unused-vars
+      const { onCloseClick, login, onLoginSuccess, forgotPassword, ...others } = this.props; //eslint-disable-line no-unused-vars
+
+      let modalContent,
+         headerText;
+      if (this.state.isLogin) {
+         headerText = "Log In";
+         modalContent = <LoginForm
+            onSubmit={this._handleLoginSubmit}
+            onChangeToForgotPassword={this._handleChangeToForgotPassword}
+            errors={this.state.errors} />;
+      }
+      else {
+         headerText = "Forgot password";
+         modalContent = <ForgotPasswordForm
+            onSubmit={this._handleForgotPasswordSubmit}
+            errors={this.state.errors}
+            onChangeToLogin={this._handleChangeToLogin}
+            forgotPasswordSuccess={this.state.forgotPasswordSuccess} />;
+      }
 
       return (
          <Modal size="tiny" {...others} closeIcon={<Icon name="close" onClick={this._onCloseClick} />}>
-            <Modal.Header content="Log In" />
+            <Modal.Header content={headerText} />
             <Modal.Content>
-               <LoginForm onSubmit={this._onSubmit} errors={this.state.errors} />
+               {modalContent}
             </Modal.Content>
          </Modal>
       );
    }
 
-   _onSubmit = (credentials) => {
+   _handleChangeToForgotPassword = () => this.setState({
+      isLogin: false,
+      errors: []
+   });
+
+   _handleChangeToLogin = () => this.setState({
+      isLogin: true,
+      errors: [],
+      forgotPasswordSuccess: false,
+   });
+
+   _handleLoginSubmit = (credentials) => {
       this.props.login(credentials).then(response => {
          if (response.data.login) {
             this.props.onLoginSuccess(response.data.login.token);
@@ -43,14 +77,32 @@ class LoginModal extends React.Component {
       }).catch(error => mutationErrorHandling(error, this._onShowError));
    }
 
+   _handleForgotPasswordSubmit = (email) => {
+      this.props.forgotPassword(email).then(response => {
+         if (response.data.forgotPassword) {
+            this.setState({
+               errors: [],
+               forgotPasswordSuccess: true,
+            });
+         }
+      }).catch(error => mutationErrorHandling(error, this._onShowError));
+   }
+
    _onShowError = (errors) => this.setState({ errors });
 
    _onCloseClick = () => {
-      this.setState({ errors: [] }, () => {
+      this.setState({
+         isLogin: true,
+         errors: [],
+         forgotPasswordSuccess: false,
+      }, () => {
          this.props.onCloseClick();
       });
    }
 
 };
 
-export default loginMutation(LoginModal);
+export default compose(
+   loginMutation,
+   forgotPasswordMutation,
+)(LoginModal);
